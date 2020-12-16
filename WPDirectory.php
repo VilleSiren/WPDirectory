317b7b38f30765faaef9cb92a6a872d6c89a915e
VilleSiren <4667917+VilleSiren@users.noreply.github.com>
2020-12-16 11:18:47 +0200

    Test

--- WPDirectory.php
+++ WPDirectory.php
@@ -4,7 +4,7 @@
  * Description:       WP Directory Service for Wordpress
  * Version:           0.0.1
  * Requires at least: 5.2
- * Requires PHP:      7.1
+ * Requires PHP:      7.2
  * Author:            Ville Siren
  * Author URI:        https://jyva.io/
  * License:           All rights reserved.
