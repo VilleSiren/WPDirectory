872d69de435d53438f37c2f42bbd49386d0c48bf
VilleJyva <72099025+VilleJyva@users.noreply.github.com>
2020-12-16 12:45:38 +0200

    Commit

--- WPDirectory.php
+++ WPDirectory.php
@@ -2,7 +2,7 @@
  * Plugin Name:       WP Directory
  * Plugin URI:        https://github.com/JyvaDigital/WPDirectory.git
  * Description:       WP Directory Service for Wordpress q
- * Version:           0.0.3
+ * Version:           0.0.4
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Ville Siren
